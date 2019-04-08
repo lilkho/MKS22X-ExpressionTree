a649ee28cac7e10ab1b3ca1fb39e083c2bfbcc29
lilkho <mkhondker@stuy.edu>
2019-04-08 14:43:17 -0400

    finshed

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -7,14 +7,22 @@ public class ExpressionTree{
   /* The sample tree would be: "(3 + (2 * 10))"     */
   public String toString(){
     /*you are to write this method*/
-    return "";
+    if (isValue()) {
+      return ""+getValue();
+    } else {
+      return "("+getLeft().toString()+" "+getOp()+" "+getRight().toString()+")";
+    }
   }
 
   /*return the expression as a postfix notation string without parenthesis*/
   /* The sample tree would be: "3 2 10 * +"     */
   public String toStringPostfix(){
     /*you are to write this method*/
-    return "";
+    if (isValue()) {
+      return ""+getValue();
+    } else {
+      return getLeft().toStringPostfix()+" "+getRight().toStringPostfix()+" "+getOp();
+    }
   }
 
   /*return the expression as a prefix notation string without parenthesis*/
@@ -22,7 +30,11 @@ public class ExpressionTree{
 
   public String toStringPrefix(){
     /*you are to write this method*/
-    return "";
+    if (isValue()) {
+      return ""+getValue();
+    } else {
+      return getOp()+" "+getLeft().toStringPrefix()+" "+getRight().toStringPrefix();
+    }
   }
 
 
@@ -31,16 +43,21 @@ public class ExpressionTree{
 
   public double evaluate(){
     /*you are to write this method*/
-    return 0.0;
-
+    if (isValue()) {
+      return getValue();
+    } else {
+      return apply(getOp(),getLeft().evaluate(),getRight().evaluate());
     }
+  }
 
 
   /*use the correct operator on both a and b, and return that value*/
   private double apply(char op, double a, double b){
-    /*you are to write this method*/
-    return 0.0;
-
+    if (op=='*') return a * b;
+    if (op=='+') return a + b;
+    if (op=='-') return a - b;
+    if (op=='/') return a / b;
+    else return 0;
     }
 
 
